329d948a95bb550f32c236d95b2b56814b98b83b
Aman-tech107 <63861606+Aman-tech107@users.noreply.github.com>
2020-05-01 19:56:27 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,7 @@
+class Pig extends BaseClass{
+    constructor(x, y) {
+      super(x, y, 50, 40);
+      this.image = loadImage("sprites/enemy.png");
+    }
+  };
+  
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Base-Class-in-Angry-Bird
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
